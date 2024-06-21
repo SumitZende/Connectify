1b113867782c9ff259e1a0ee6b88dbba0cc42369
sumitzende <sumitzende652@gmail.com>
2024-06-22 04:35:56 +0530

    fixing warning of model value in GroupListIte.vue

--- resources/js/Components/app/GroupListItem.vue
+++ resources/js/Components/app/GroupListItem.vue
@@ -6,7 +6,7 @@ import TextInput from "@/Components/TextInput.vue";
 </script>
 
 <template>
-  <TextInput :model-value="serachKeyword" placeholder="type for search " class="w-full  mt-4"/>
+  <TextInput  placeholder="type for search " class="w-full  mt-4"/>
   <div class="mt-3  h-[200px]  lg:flex-1 overflow-auto ">
     <div v-if="false" class="text-gray-400  text-center p-6">
       You are not joined to any groups
